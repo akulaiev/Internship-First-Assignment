b34d74d2bc62d53c34ae4984a5ef0338b2877bda
akulaiev <kulaeva.a@ukr.net>
2020-11-16 23:25:56 +0200

    initial

---
+++ firstAssignment/ManageFunctionsSignatures.swift
@@ -0,0 +1,44 @@
+//
+//  ManageFunctionsSignatures.swift
+//  firstAssignment
+//
+//  Created by Anna Kulaieva on 15.11.2020.
+//
+
+import Foundation
+
+enum FunctionNamesEnum: String {
+    case isSimilar = "Is Similar", permulations = "Permutations",
+         isSimpleNumber = "Is Simple Number", averageNumber = "Average Number",
+         tripleLetterWordsCpint = "Triple letter words count"
+}
+
+struct AllFunctions: Codable {
+    let allFunctions: [FunctionSignitureInfo]
+    let allFunctionNames: [String]
+}
+
+struct FunctionSignitureInfo: Codable {
+    let functionName: String
+    let inputFieldsNumber: Int
+    let inputNames: [String]
+}
+
+class FunctionsSignatures {
+    
+    var functionInfo: AllFunctions?
+    
+    func readFunctionsInfoJSON() {
+        if let url = Bundle.main.url(forResource: "functionsInfo", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(AllFunctions.self, from: data)
+                functionInfo = jsonData
+            }
+            catch {
+                print("error:\(error)")
+            }
+        }
+    }
+}
---
+++ firstAssignment/ViewControllers/TasksListViewController.swift
@@ -0,0 +1,57 @@
+//
+//  TasksListViewController.swift
+//  firstAssignment
+//
+//  Created by Anna Kulaieva on 14.11.2020.
+//
+
+import UIKit
+
+class TasksListViewController: UIViewController {
+
+    @IBOutlet weak var tasksListPickerView: UIPickerView!
+    @IBOutlet weak var testFunctionButton: UIButton!
+    
+    let functions = FunctionsSignatures()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        functions.readFunctionsInfoJSON()
+        testFunctionButton.layer.cornerRadius = 5
+        tasksListPickerView.delegate = self
+        tasksListPickerView.dataSource = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let taskImplementationVC = segue.destination as! TaskImplementationViewController
+        guard let functionsInfo = functions.functionInfo else {
+            print("An error reading functions info json file has occured")
+            return
+        }
+        taskImplementationVC.selectedFunctionName = FunctionNamesEnum(rawValue: functionsInfo.allFunctions[tasksListPickerView.selectedRow(inComponent: 0)].functionName)
+        taskImplementationVC.pickedFunctionsSignature = functionsInfo.allFunctions[tasksListPickerView.selectedRow(inComponent: 0)]
+        taskImplementationVC.allFunctionNames = functionsInfo.allFunctionNames
+    }
+}
+
+extension TasksListViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        guard let functionsInfo = functions.functionInfo else {
+            return 0
+        }
+        return functionsInfo.allFunctionNames.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        guard let functionsInfo = functions.functionInfo else {
+            return nil
+        }
+        return functionsInfo.allFunctionNames[row]
+    }
+}
